dcb87443042497c4edb1c4259b1537310d5ff847
CleanCode <73321946+CleancodeGitHub@users.noreply.github.com>
2024-03-03 18:03:02 -0700

    Update index.js

--- family basket-app/index.js
+++ family basket-app/index.js
@@ -3,12 +3,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-    databaseURL: "Some key from Firebase libraries.com"
+    databaseURL: "Some key from Firebase libraries.com" //Enter the key from the Firebase database
 };
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
-const shoppingListInDB = ref(database, "shoppingList");
+const shoppingListInDB = ref(database, "shoppingList"); 
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
